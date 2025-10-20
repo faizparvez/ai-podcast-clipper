e8f46c076bb547030230ad8d00e7ef789642633e
faizparvez <151218353+faizparvez@users.noreply.github.com>
2025-10-21 02:26:34 +0530

    created steps in inngest

--- frontend/src/inngest/functions.ts
+++ frontend/src/inngest/functions.ts
@@ -1,12 +1,12 @@
 import { env } from "~/env";
 import { inngest } from "./client";
-// import { db } from "~/server/db";
-// import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
+import { db } from "~/server/db";
+import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
 
 export const processVideo = inngest.createFunction(
   {
     id: "process-video",
-    // retries: 1,
+    retries: 1,
     concurrency: {
       limit: 1,
       key: "event.data.userId",
@@ -14,150 +14,151 @@ export const processVideo = inngest.createFunction(
   },
   { event: "process-video-events" },
   async ({ event, step }) => {
-    // const { uploadedFileId } = event.data as {
+    const { uploadedFileId } = event.data;
+    // as {
     //   uploadedFileId: string;
     //   userId: string;
     // };
 
-    // try {
-    //   const { userId, credits, s3Key } = await step.run(
-    //     "check-credits",
-    //     async () => {
-    //       const uploadedFile = await db.uploadedFile.findUniqueOrThrow({
-    //         where: {
-    //           id: uploadedFileId,
-    //         },
-    //         select: {
-    //           user: {
-    //             select: {
-    //               id: true,
-    //               credits: true,
-    //             },
-    //           },
-    //           s3Key: true,
-    //         },
-    //       });
+    try {
+      const { userId, credits, s3Key } = await step.run(
+        "check-credits",
+        async () => {
+          const uploadedFile = await db.uploadedFile.findUniqueOrThrow({
+            where: {
+              id: uploadedFileId,
+            },
+            select: {
+              user: {
+                select: {
+                  id: true,
+                  credits: true,
+                },
+              },
+              s3Key: true,
+            },
+          });
 
-    //       return {
-    //         userId: uploadedFile.user.id,
-    //         credits: uploadedFile.user.credits,
-    //         s3Key: uploadedFile.s3Key,
-    //       };
-    //     },
-    //   );
+          return {
+            userId: uploadedFile.user.id,
+            credits: uploadedFile.user.credits,
+            s3Key: uploadedFile.s3Key,
+          };
+        },
+      );
 
-    //   if (credits > 0) {
-    //     await step.run("set-status-processing", async () => {
-    //       await db.uploadedFile.update({
-    //         where: {
-    //           id: uploadedFileId,
-    //         },
-    //         data: {
-    //           status: "processing",
-    //         },
-    //       });
-    //     });
-
-    await step.run("call-modal-endpoint", async () => {
-        await step.fetch(env.PROCESS_VIDEO_ENDPOINT, {
-          method: "POST",
-          body: JSON.stringify({ s3_key: "test1/mi65min.mp4" }),
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${env.PROCESS_VIDEO_ENDPOINT_AUTH}`,
-          },
+      if (credits > 0) {
+        await step.run("set-status-processing", async () => {
+          await db.uploadedFile.update({
+            where: {
+              id: uploadedFileId,
+            },
+            data: {
+              status: "processing",
+            },
+          });
         });
-    })
 
-        // const { clipsFound } = await step.run(
-        //   "create-clips-in-db",
-        //   async () => {
-        //     const folderPrefix = s3Key.split("/")[0]!;
+        await step.run("call-modal-endpoint", async () => {
+          await step.fetch(env.PROCESS_VIDEO_ENDPOINT, {
+            method: "POST",
+            body: JSON.stringify({ s3_key: s3Key }),
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${env.PROCESS_VIDEO_ENDPOINT_AUTH}`,
+            },
+          });
+        });
 
-        //     const allKeys = await listS3ObjectsByPrefix(folderPrefix);
+        const { clipsFound } = await step.run(
+          "create-clips-in-db",
+          async () => {
+            const folderPrefix = s3Key.split("/")[0]!;
 
-        //     const clipKeys = allKeys.filter(
-        //       (key): key is string =>
-        //         key !== undefined && !key.endsWith("original.mp4"),
-        //     );
+            const allKeys = await listS3ObjectsByPrefix(folderPrefix);
 
-        //     if (clipKeys.length > 0) {
-        //       await db.clip.createMany({
-        //         data: clipKeys.map((clipKey) => ({
-        //           s3Key: clipKey,
-        //           uploadedFileId,
-        //           userId,
-        //         })),
-        //       });
-        //     }
+            const clipKeys = allKeys.filter(
+              (key): key is string =>
+                key !== undefined && !key.endsWith("original.mp4"),
+            );
 
-        //     return { clipsFound: clipKeys.length };
-        //   },
-        // );
+            if (clipKeys.length > 0) {
+              await db.clip.createMany({
+                data: clipKeys.map((clipKey) => ({
+                  s3Key: clipKey,
+                  uploadedFileId,
+                  userId,
+                })),
+              });
+            }
 
-        // await step.run("deduct-credits", async () => {
-        //   await db.user.update({
-        //     where: {
-        //       id: userId,
-        //     },
-        //     data: {
-        //       credits: {
-        //         decrement: Math.min(credits, clipsFound),
-        //       },
-        //     },
-        //   });
-        // });
+            return { clipsFound: clipKeys.length };
+          },
+        );
 
-        // await step.run("set-status-processed", async () => {
-        //   await db.uploadedFile.update({
-        //     where: {
-        //       id: uploadedFileId,
-        //     },
-        //     data: {
-        //       status: "processed",
-        //     },
-        //   });
-        // });
-  //     } 
-  //     else {
-  //       await step.run("set-status-no-credits", async () => {
-  //         await db.uploadedFile.update({
-  //           where: {
-  //             id: uploadedFileId,
-  //           },
-  //           data: {
-  //             status: "no credits",
-  //           },
-  //         });
-  //       });
-  //     }
-  //   } catch (error: unknown) {
-  //     await db.uploadedFile.update({
-  //       where: {
-  //         id: uploadedFileId,
-  //       },
-  //       data: {
-  //         status: "failed",
-  //       },
-  //     });
-  //   }
+        await step.run("deduct-credits", async () => {
+          await db.user.update({
+            where: {
+              id: userId,
+            },
+            data: {
+              credits: {
+                decrement: Math.min(credits, clipsFound),
+              },
+            },
+          });
+        });
+
+        await step.run("set-status-processed", async () => {
+          await db.uploadedFile.update({
+            where: {
+              id: uploadedFileId,
+            },
+            data: {
+              status: "processed",
+            },
+          });
+        });
+      } else {
+        await step.run("set-status-no-credits", async () => {
+          await db.uploadedFile.update({
+            where: {
+              id: uploadedFileId,
+            },
+            data: {
+              status: "no credits",
+            },
+          });
+        });
+      }
+    } 
+    catch (error: unknown) {
+      await db.uploadedFile.update({
+        where: {
+          id: uploadedFileId,
+        },
+        data: {
+          status: "failed",
+        },
+      });
+    }
   },
 );
 
-// async function listS3ObjectsByPrefix(prefix: string) {
-//   const s3Client = new S3Client({
-//     region: env.AWS_REGION,
-//     credentials: {
-//       accessKeyId: env.AWS_ACCESS_KEY_ID,
-//       secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
-//     },
-//   });
+async function listS3ObjectsByPrefix(prefix: string) {
+  const s3Client = new S3Client({
+    region: env.AWS_REGION,
+    credentials: {
+      accessKeyId: env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+    },
+  });
 
-//   const listCommand = new ListObjectsV2Command({
-//     Bucket: env.S3_BUCKET_NAME,
-//     Prefix: prefix,
-//   });
+  const listCommand = new ListObjectsV2Command({
+    Bucket: env.S3_BUCKET_NAME,
+    Prefix: prefix,
+  });
 
-//   const response = await s3Client.send(listCommand);
-//   return response.Contents?.map((item) => item.Key).filter(Boolean) ?? [];
-// }
+  const response = await s3Client.send(listCommand);
+  return response.Contents?.map((item) => item.Key).filter(Boolean) ?? [];
+}
