e5048e5d58b54b8271aa5d53ac528ed0723ab407
faizparvez <151218353+faizparvez@users.noreply.github.com>
2025-10-21 02:17:44 +0530

    imported shadcn components

--- frontend/src/components/signup-form.tsx
+++ frontend/src/components/signup-form.tsx
@@ -11,7 +11,6 @@ import {
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
-import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
